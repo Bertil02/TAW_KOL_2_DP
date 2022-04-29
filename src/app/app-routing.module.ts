b519006ad13f634e83b05e5b5ed181dd18e08fbe
dominikpochron <dominik27p@gmail.com>
2022-04-29 17:16:18 +0200

    kolowkium

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {OrdersDPComponent} from "./components/orders-dp/orders-dp.component";
+import {OrdersDetailsDPComponent} from "./components/orders-details-dp/orders-details-dp.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OrdersDPComponent,
+  },
+  {
+    path: 'posts/:id',
+    component: OrdersDetailsDPComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersDPComponent } from './components/orders-dp/orders-dp.component';
+import { OrdersItemDPComponent } from './components/orders-item-dp/orders-item-dp.component';
+import { OrdersDetailsDPComponent } from './components/orders-details-dp/orders-details-dp.component';
+import {DataService} from "./services/data.service";
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersDPComponent,
+    OrdersItemDPComponent,
+    OrdersDetailsDPComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
